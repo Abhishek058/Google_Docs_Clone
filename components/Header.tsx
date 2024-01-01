fb97642c3ac02883c51872e77d81d7fbbee13205
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2024-01-01 11:08:02 +0530

    Header section completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,17 +2,20 @@ import Menu from "@mui/icons-material/Menu";
 import DescriptionIcon from "@mui/icons-material/Description";
 import { Button } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import AppsIcon from "@mui/icons-material/Apps";
 
 export default function Header() {
   return (
-    <div className="w-full flex items-center shadow-md px-1 py-4">
-      <Button className="rounded-full my-4">
-        <Menu className="text-black md:text-3xl" />
-      </Button>
-      <DescriptionIcon className="text-blue-500 text-4xl md:text-5xl" />
-      <h1 className="text-gray-600 text-xl md:text-2xl">Docs</h1>
+    <header className="w-full flex items-center justify-between shadow-md px-1">
+      <div className="flex items-center">
+        <Button className="rounded-full my-4">
+          <Menu className="text-black md:text-3xl" />
+        </Button>
+        <DescriptionIcon className="text-blue-500 text-4xl md:text-5xl" />
+        <h1 className="text-gray-600 text-xl md:text-2xl">Docs</h1>
+      </div>
 
-      <div className="sm:w-1/2 sm:px-2 sm:py-2 sm:flex sm:items-center sm:ml-4 sm:border-2 sm:rounded-lg sm:bg-gray-100 sm:text-gray-600 hidden">
+      <div className="sm:w-1/2 sm:px-2 sm:py-2 sm:flex sm:items-center sm:ml-4 sm:border-2 sm:rounded-lg sm:bg-gray-100 sm:text-gray-500 hidden focus-within:shadow-md focus-within:text-black">
         <SearchIcon />
         <input
           type="text"
@@ -20,6 +23,16 @@ export default function Header() {
           className="flex flex-grow outline-none bg-transparent ml-2"
         />
       </div>
-    </div>
+      <div className="flex items-center mr-4 gap-3">
+        <Button className="rounded-full">
+          <AppsIcon className="text-gray-500 text-3xl" />
+        </Button>
+        <img
+          src="https://lh3.googleusercontent.com/ogw/ANLem4ZRiMwS5EAPHvNmHa3ay2op6XaVl3M4D2UxZOc8=s64-c-mo"
+          alt="user image"
+          className="w-12 h-12 rounded-full cursor-pointer"
+        />
+      </div>
+    </header>
   );
 }
